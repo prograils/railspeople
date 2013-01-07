7b2349d2a7d26f7444555099a10578731053ee78
Rafał Trojanowski <rafal@prograils.com>
2013-01-07 11:22:43 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railspeople::Application.config.secret_key_base = 'ea4f8d5bd10d418b77cc8b260a104565107f10200cafd2c2622144f894c05a16ec5232f24471d138df64a4b9ced0dd5adc4f2ab28c15993ef016c6ef37c192e5'
