d91fda67ee135558a641009c009dccfcc47d8a00
Rafał Trojanowski <rafal@prograils.com>
2013-01-07 11:50:06 +0100

    user model with devise, home#index

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'devise'
 gem 'rails',     github: 'rails/rails'
 gem 'arel',      github: 'rails/arel'
 gem 'activerecord-deprecated_finders', github: 'rails/activerecord-deprecated_finders'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Railspeople::Application.routes.draw do
+  get "home/index"
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
