c82bde65fd1f21661d84325def30a0a1bcd645d6
Rafał Trojanowski <rafal@prograils.com>
2013-01-08 12:59:04 +0100

    migrations, sign up form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+
+  def new 
+    @json = User.new.to_gmaps4rails
+    super
+  end
+
   def resource_params
     params.require(:user).permit(:email, :password, :password_confirmation, :latitude, :longitude, :country)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130107131319) do
+ActiveRecord::Schema.define(version: 20130108112349) do
 
   create_table "users", force: true do |t|
     t.string   "email",                              default: "", null: false
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 20130107131319) do
     t.string   "country"
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
