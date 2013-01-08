4fe2b09f12de1d6fc1bdfdd86a3299b15700d5e4
Rafał Trojanowski <rafal@prograils.com>
2013-01-08 15:56:21 +0100

    fix devise in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Railspeople::Application.routes.draw do
+  devise_for :users, :controllers => {:registrations => "users/registrations", :passwords => "users/passwords"}
   resources :users, :only => :show
   get "search/index"
   get "about/index"
   get "home/index"
-  devise_for :users, :controllers => {:registrations => "users/registrations", :passwords => "users/passwords"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
