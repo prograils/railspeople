ce4a3d9cdf3eef3f4683c010eb0fd643a318275a
Rafał Trojanowski <rafal@prograils.com>
2013-01-11 17:01:59 +0100

    simple search by username

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def index
+    q = "%#{params[:q]}%"
+    @results = []
+    if q.length >= 5
+      users = User.where("username like ?", q)
+      @results = users.all
+      @json = @results.to_gmaps4rails
+    end
   end
 end
