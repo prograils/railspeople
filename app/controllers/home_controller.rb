498f16b2270c7717bd0d3467db73e1e9f79ba158
Rafał Trojanowski <rafal@prograils.com>
2013-01-11 12:37:53 +0100

    removed unwanted line

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,6 @@ class HomeController < InheritedResources::Base
       @selected_country = Country.find_by printable_name: params[:selected] 
       scope = User.where(:country_id => @selected_country.id)
       @json = scope.to_gmaps4rails
-      @people_count = scope.count
       @people = scope.paginate(:page => params[:page])
     else
       @json = User.all.to_gmaps4rails
