05b19edcc67e4b6d7f24114ef55c0a2f808b353c
justi <justyna@galdomedia.pl>
2013-01-11 14:16:16 +0100

    home view: order countries by users_count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < InheritedResources::Base
   def index
     if params[:selected]
-      @selected_country = Country.find_by printable_name: params[:selected] 
+      @selected_country = Country.find_by printable_name: params[:selected]
       scope = User.where(:country_id => @selected_country.id)
       @json = scope.to_gmaps4rails
       @people = scope.paginate(:page => params[:page])
     else
       @json = User.all.to_gmaps4rails
     end
-    @countries = Country.all
+    @countries = Country.order_by_users.all
   end
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,4 @@
 class Country < ActiveRecord::Base
   has_many :users
+  scope :order_by_users, -> { order('users_count DESC')}
 end
