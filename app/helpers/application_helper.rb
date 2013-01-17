a257fe493cdaa297a3ac32a0289749201fd4b2dc
Rafał Trojanowski <rafal@prograils.com>
2013-01-17 13:27:55 +0100

    user#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
-  def email_is_visible?
-    user_signed_in? && @user.email_privacy == 1 || @user.email_privacy == 2
+  def email_is_visible?(user)
+    user_signed_in? && user.email_privacy == 1 || user.email_privacy == 2 || user_signed_in? && current_user?(user)
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+  
+  def im_details_visible?(user)
+    user.im_privacy == true || user_signed_in? && user.im_privacy == false
   end
 end
