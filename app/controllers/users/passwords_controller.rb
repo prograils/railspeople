58e497362306775fa791615c1c3880fe58aee24d
Jarek Jeleniewicz <jarek@galdomedia.pl>
2013-02-04 13:06:44 +0100

    forgot password fix

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,4 +1,9 @@
 class Users::PasswordsController < Devise::PasswordsController
+  def new
+    super
+  end
+
+
   def resource_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
