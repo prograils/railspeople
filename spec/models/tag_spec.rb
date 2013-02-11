5befe41159d5c56292962e9fe7bfe0337d91ecc9
Rafał Trojanowski <rafal@prograils.com>
2013-02-11 17:21:57 +0100

    small improvement

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Tag do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:all)  { @user = FactoryGirl.create(:user) }
+  
+  it "should save only unique tags from tag_names" do
+    @user.tag_names = "raz dwa trzy trzy"
+    @user.save
+    
+    Tag.count.should == 3
+    Tagging.count.should == 3
+    @user.tags.count.should == 3
+  end
+
+  it "should destroy record in tagging table after change tags" do
+    @user.tag_names = "trzy cztery"
+    @user.save  
+    
+    @user.tags.count.should == 2 
+    Tag.count.should == 4
+    Tagging.count.should == 2 
+  end
 end
