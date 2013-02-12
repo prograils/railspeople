469091991ea3ada2fa8e5c3e111fac0d1567edc9
justi <justyna@galdomedia.pl>
2013-02-12 16:43:41 +0100

    fix users_controller_spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe UsersController do
+describe "UsersController" do
   before(:each) do
     @user = FactoryGirl.create(:user)
     @user_with_email_for_no_one = FactoryGirl.create(:user, :email_privacy => 0)
@@ -9,19 +9,57 @@ describe UsersController do
   end
 
   describe "GET 'show'" do
+    context 'user not logged in' do
 
-    context 'user signed in' do
-      it "returns http success for non logged in user" do
-        visit user_path(@user)
+      it "show page content" do
+        visit "/users/#{@user.id}"
+        page.should have_content "People near"
         page.should have_content @user.first_name
+      end
+
+      it "don't show user email if is for no one" do
+        visit "/users/#{@user_with_email_for_no_one.id}"
+        page.should_not have_content @user.email
+      end
+
+      it "don't show user email if is for logged in only" do
+        visit "/users/#{@user_with_email_for_logged_in.id}"
+        page.should_not have_content @user.email
+      end
+
+      it "show user email if is for anyone" do
+        visit "/users/#{@user_with_email_for_anyone.id}"
+        page.should_not have_content @user.email
       end
     end
 
-    context 'user not signed in' do
-      it "returns http success if user is logged in" do
-        sign_in @user
-        visit user_path(@user)
+    context 'user logged in' do
+      before(:each) { login_as(@user, :scope => :user) }
+
+      it "show page content" do
+        visit "/users/#{@user.id}"
         page.should have_content "People near"
+        page.should have_content @user.first_name
+      end
+
+      it "show user email on his page" do
+        visit "/users/#{@user.id}"
+        page.should have_content @user.email
+      end
+
+      it "don't show user email if is for no one" do
+        visit "/users/#{@user_with_email_for_no_one.id}"
+        page.should_not have_content @user_with_email_for_no_one.email
+      end
+
+      it "show user email if is for logged in only" do
+        visit "/users/#{@user_with_email_for_logged_in.id}"
+        page.should have_content @user_with_email_for_logged_in.email
+      end
+
+      it "show user email if is for anyone" do
+        visit "/users/#{@user_with_email_for_anyone.id}"
+        page.should have_content @user_with_email_for_anyone.email
       end
     end
   end
