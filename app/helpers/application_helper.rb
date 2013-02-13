607c93cf0ea969eff7226de8936d67269b068ac8
Jarek Jeleniewicz <jarek@galdomedia.pl>
2013-02-13 15:47:28 +0100

    add gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ module ApplicationHelper
   def im_details_visible?(user)
     user.im_privacy == true || user_signed_in? && user.im_privacy == false
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?d=mm"
+  end
 end
