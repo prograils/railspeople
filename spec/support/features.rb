da07b879999492e63c6cc8ca0a5e082503bff880
justi <justyna@galdomedia.pl>
2013-03-20 08:40:05 +0100

    config travis cd..

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -1,4 +1,4 @@
-#Dir[Rails.root.join(File.expand_path('features', __FILE__), "*.rb")].each {|f| require f}
+Dir[Rails.root.join(File.expand_path('../features', __FILE__), "*.rb")].each {|f| require f}
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
 end
